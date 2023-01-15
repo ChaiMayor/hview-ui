7c3ed30f8788c7bda3811c20f168739778108639
leoyongyuan <1359195795@qq.com>
2023-01-15 21:25:55 +0800

    feat: 全局样式的更新，主页面的更新

--- examples/app.vue
+++ examples/app.vue
@@ -1,63 +1,39 @@
 <template>
-  <!-- <div class="wrapper">
+  <div class="wrapper">
     <div class="sidebar-nav">
       <div class="sidebar-menu">
         <teleport to="body">
-          <router-link to="/" title="RelaxPlus">
+          <router-link to="/" title="HviewPlus">
+            <div class="logo-img"></div>
           </router-link>
         </teleport>
+        
+        <!-- 右侧边栏导航区域 -->
+        <div v-for="(menu, i) in navBar" class="menu" :key="i">
+          <dl><dt># 常用组件</dt></dl>
+          <router-link :to="menu.routePath">
+            {{menu.title}}
+          </router-link>
+        </div>
       </div>
     </div>
-  </div> -->
-    <div>
-        <h-button>默认按钮</h-button>
-        <h-button type="primary">主要按钮</h-button>
-        <h-button type="success">成功按钮</h-button>
-        <h-button type="info">信息按钮</h-button>
-        <h-button type="warning">警告按钮</h-button>
-        <h-button type="danger">危险按钮</h-button>
-        <h-button type="text">文字按钮</h-button>
-        <br>
-        <br>
-        <h-button plain>朴素按钮</h-button>
-        <h-button type="primary" plain>主要按钮</h-button>
-        <h-button type="success" plain>成功按钮</h-button>
-        <h-button type="info" plain>信息按钮</h-button>
-        <h-button type="warning" plain>警告按钮</h-button>
-        <h-button type="danger" plain>危险按钮</h-button>
-        <br>
-        <br>
-        <h-button round>圆角按钮</h-button>
-        <h-button type="primary" round>主要按钮</h-button>
-        <h-button type="success" round>成功按钮</h-button>
-        <h-button type="info" round>信息按钮</h-button>
-        <h-button type="warning" round>警告按钮</h-button>
-        <h-button type="danger" round>危险按钮</h-button>
-        <br>
-        <br>
-        <h-button disabled>禁用按钮</h-button>
-        <h-button type="primary" disabled>主要按钮</h-button>
-        <h-button type="success" disabled>成功按钮</h-button>
-        <h-button type="info" disabled>信息按钮</h-button>
-        <h-button type="warning" disabled>警告按钮</h-button>
-        <h-button type="danger" disabled>危险按钮</h-button>
-        <br>
-        <br>
-        <h-button disabled>禁用按钮</h-button>
-        <h-button type="primary" disabled plain>主要按钮</h-button>
-        <h-button type="success" disabled plain>成功按钮</h-button>
-        <h-button type="info" disabled plain>信息按钮</h-button>
-        <h-button type="warning" disabled plain>警告按钮</h-button>
-        <h-button type="danger" disabled plain>危险按钮</h-button>
-        <br>
-        <br>
-        <h-button>默认按钮</h-button>
-        <h-button size="medium">中等按钮</h-button>
-        <h-button size="small">小型按钮</h-button>
-        <h-button size="mini">超小按钮</h-button>
-    </div>
+    <View />
+  </div>
 </template>
-<script lang="ts" setup>
+<script lang="ts">
+import { data } from './router/data'
+import View from './views/View.vue'
+export default {
+  components: {
+    View,
+  },
+  setup() {
+    const navBar = data
+    return {
+      navBar
+    }
+  },
+}
 </script>
 <style lang="less">
 </style>
