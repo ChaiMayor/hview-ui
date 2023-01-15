7bc8efb21bfb8ca1f0857501df5381a14a52af62
leoyongyuan <1359195795@qq.com>
2023-01-15 21:14:50 +0800

    feat: 配置路由

---
+++ examples/router/data.ts
@@ -0,0 +1,16 @@
+interface Base {
+  title: string,
+  componentName: string,
+  componentPath: string,
+  routePath: string
+}
+
+export const data : Array<Base> = [ 
+  {
+    title: 'Button 按钮',
+    componentName: 'button',
+    componentPath: 'button/View/index',
+    routePath: '/button'
+  },
+]
+
