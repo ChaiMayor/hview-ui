b4234de96877cd1bf0e29bccd02bb2941a23c5db
leoyongyuan <1359195795@qq.com>
2023-01-15 21:12:43 +0800

    build: 增加路由和其它一些配置

--- examples/main.ts
+++ examples/main.ts
@@ -1,8 +1,9 @@
 import {createApp} from 'vue'
 import App from './app.vue'
+import router from './router'
 import './style/base.less'
 
 import HViewPlus from '../packages/components/src'
 const app = createApp(App)
 
-app.use(HViewPlus).mount('#app')
+app.use(HViewPlus).use(router).mount('#app')
