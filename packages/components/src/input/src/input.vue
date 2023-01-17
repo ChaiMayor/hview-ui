de292d84ee39e70da6a55ad4666845c21a7b843f
leoyongyuan <1359195795@qq.com>
2023-01-17 23:14:48 +0800

    feat: 初始化input组件

---
+++ packages/components/src/input/src/input.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="h-form-input"></div>
+</template>
+
+<script setup lang="ts">
+import "../style/index.less";
+import { defineComponent } from "vue";
+</script>
+
+<script lang="ts">
+export default defineComponent({
+  name: "HInput",
+});
+</script>
