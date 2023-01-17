52d84bf361c4ccfe891e691ea77231dc1ffd87d7
ChaiMayor <w2521630545@163.com>
2023-01-17 09:19:31 +0800

    feat: 添加了vitepress打包功能

--- site/docs/.vitepress/config/plugins.ts
+++ site/docs/.vitepress/config/plugins.ts
@@ -1,16 +1,16 @@
-import MarkdownIt from 'markdown-it'
+import MarkdownIt from "markdown-it";
 // import MarkdownItContainer from 'markdown-it-container'
-import TableContainer from './table'
+import TableContainer from "./table";
 // import type { Token } from 'markdown-it'
 
 // https://markdown-it.docschina.org/#%E7%94%A8%E6%B3%95%E7%A4%BA%E4%BE%8B
 // https://juejin.cn/post/6844903688536850440
 const markdown: MarkdownIt = MarkdownIt({
-  breaks: true
-})
+  breaks: true,
+});
 
 export const mdPlugin = (md: MarkdownIt) => {
-  md.use(TableContainer)
+  md.use(TableContainer);
   // md.use(MarkdownItContainer, 'demo', {
   //   validate(params) {
   //     return !!params.trim().match(/^demo\s*(.*)$/)
@@ -47,4 +47,4 @@ export const mdPlugin = (md: MarkdownIt) => {
   //   //   return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
   //   // }
   // })
-}
+};
--- site/docs/.vitepress/utils/sidebar.ts
+++ site/docs/.vitepress/utils/sidebar.ts
@@ -1,58 +1,57 @@
 export const sidebar = {
-  '/docs/': [
+  "/docs/": [
     {
-      text: '基础',
-      link: '/docs/',
+      text: "基础",
+      link: "/docs/",
       items: [
-        { text: '安装', link: '/docs/installation' },
-        { text: '快速上手', link: '/docs/quickstart' }
-      ]
+        { text: "安装", link: "/docs/installation" },
+        { text: "快速上手", link: "/docs/quickstart" },
+      ],
     },
     {
-      text: '进阶',
-      link: '/docs/',
+      text: "进阶",
+      link: "/docs/",
       items: [
-        { text: '国际化', link: '/docs/' },
-        { text: '主题', link: '/docs/' },
-        { text: '更新日志', link: '/docs/' },
-      ]
+        { text: "国际化", link: "/docs/" },
+        { text: "主题", link: "/docs/" },
+        { text: "更新日志", link: "/docs/" },
+      ],
     },
     {
-      text: '开发',
-      link: '/docs/',
-      items: [
-        { text: '开发指南', link: '/docs/' },
-      ]
-    }
+      text: "开发",
+      link: "/docs/",
+      items: [{ text: "开发指南", link: "/docs/" }],
+    },
   ],
-  '/components/': [
+  "/components/": [
     {
-      text: '基础组件',
-      link: '/components/',
+      text: "基础组件",
+      link: "/components/",
       items: [
-        { text: 'Button 按钮', link: '/components/button' },
-        { text: 'Icon 图标', link: '/components/icon' },
-        { text: 'Link 链接', link: '/components/link' },
-        { text: 'Layout 布局', link: '/components/layout' }
-      ]
-    }, {
-      text: 'Form 表单组件',
-      link: '/components/',
+        { text: "Button 按钮", link: "/components/button" },
+        { text: "Icon 图标", link: "/components/icon" },
+        { text: "Link 链接", link: "/components/link" },
+        { text: "Layout 布局", link: "/components/layout" },
+      ],
+    },
+    {
+      text: "Form 表单组件",
+      link: "/components/",
       items: [
-        { text: 'Input 输入框', link: '/components/input' },
-        { text: 'Shake 抖动', link: '/components/shake' },
-        { text: 'Radio 单选框', link: '/components/radio' },
-        { text: 'CheckBox 多选框', link: '/components/checkbox' },
-        { text: 'Select 选择器', link: '/components/select' },
-        { text: 'Cascader 级联选择器', link: '/components/cascader' },
-        { text: 'Switch 开关', link: '/components/switch' },
-        { text: 'Slider 滑块', link: '/components/slider' },
-        { text: 'TimePicker 时间选择器', link: '/components/timepicker' },
-        { text: 'DatePicker 日期选择器', link: '/components/datepicker' },
-        { text: 'DateTimePicker 日期时间选择器', link: '/components/datetimepicker' },
-        { text: 'Upload 上传', link: '/components/upload' },
-        { text: 'Rate 评分', link: '/components/rate' }
-      ]
-    }
-  ]
-}
+        { text: "Input 输入框", link: "/components/input" },
+        { text: "Shake 抖动", link: "/components/shake" },
+        { text: "Radio 单选框", link: "/components/radio" },
+        { text: "CheckBox 多选框", link: "/components/checkbox" },
+        { text: "Select 选择器", link: "/components/select" },
+        { text: "Cascader 级联选择器", link: "/components/cascader" },
+        { text: "Switch 开关", link: "/components/switch" },
+        { text: "Slider 滑块", link: "/components/slider" },
+        { text: "TimePicker 时间选择器", link: "/components/timepicker" },
+        { text: "DatePicker 日期选择器", link: "/components/datepicker" },
+        { text: "DateTimePicker 日期时间选择器", link: "/components/datetimepicker" },
+        { text: "Upload 上传", link: "/components/upload" },
+        { text: "Rate 评分", link: "/components/rate" },
+      ],
+    },
+  ],
+};
