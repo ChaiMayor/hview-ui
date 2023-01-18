2a3507785b574bfb3c66ade25eefd296962dfdc1
leoyongyuan <1359195795@qq.com>
2023-01-18 11:23:47 +0800

    feat: 完成input组件的基础功能

--- packages/components/src/input/src/input.vue
+++ packages/components/src/input/src/input.vue
@@ -1,10 +1,28 @@
 <template>
-  <div class="h-form-input"></div>
+  <div class="h-form-input">
+    <input class="h-input" :type="type" :value="text" @input="handerInput" />
+  </div>
 </template>
 
 <script setup lang="ts">
 import "../style/index.less";
-import { defineComponent } from "vue";
+import { InputProps } from "./input";
+import { defineComponent, toRefs, ref, watchEffect } from "vue";
+
+const props = defineProps(InputProps);
+const emit = defineEmits(["update:modelValue"]);
+const { modelValue } = toRefs(props);
+const text = ref();
+
+watchEffect(() => {
+  // text.value = modelValue!.value,
+  text.value = modelValue! ? modelValue.value : "";
+});
+
+const handerInput = (e: any) => {
+  text.value = e ? e.target.value : "";
+  emit("update:modelValue", text.value);
+};
 </script>
 
 <script lang="ts">
