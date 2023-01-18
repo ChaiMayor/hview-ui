cf6e03779bce9b7525c42bd350cbd14b9b0126ef
leoyongyuan <1359195795@qq.com>
2023-01-18 16:16:05 +0800

    chore: 测试input中icon的使用

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
-    <h-button type="success" :round="true">Success</h-button>
-    <h-input v-model="value" placeholder="Please input"></h-input>
-    {{ value }}
+    <h-button type="success"><h-icon name="edit"></h-icon></h-button>
   </div>
 </template>
 <script lang="ts" setup>
 import { ref } from "vue";
+import "./base.less";
 const value = ref();
 </script>
