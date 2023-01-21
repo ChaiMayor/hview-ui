3fe7a64c44ed29bb64e383186504f5b8991e5bc8
leoyongyuan <1359195795@qq.com>
2023-01-21 12:04:15 +0800

    fix: 解决组件脱离文档流后渲染失败的bug

--- packages/components/src/tooltip/src/tooltip.vue
+++ packages/components/src/tooltip/src/tooltip.vue
@@ -1,125 +1,125 @@
-<script lang="ts">
-import { 
-  getCurrentInstance, 
-  defineComponent, 
-  useSlots, 
-  onMounted, 
-  toRefs, 
-  ref, 
-  watchEffect, 
-  nextTick 
-} from "vue";
+<template>
+  <span>
+    <slot></slot>
+  </span>
+</template>
+<script setup lang="ts">
 import "../style/index.less";
+import { getCurrentInstance, defineComponent, useSlots, onMounted, toRefs, ref, watchEffect, nextTick } from "vue";
+import { TooltipProps } from "./tooltip";
+
+const props = defineProps(TooltipProps);
+const slots = useSlots();
+// 获取插槽元素
+function getFirstElement() {
+  const slotsDefault = slots.default &&  slots.default()
+  if (!Array.isArray(slotsDefault)) return null;
+  console.log(slotsDefault)
+  let element = null;
+  for (let index = 0; index < slotsDefault.length; index++) {
+    if (slotsDefault[index] && slotsDefault[index].type) {
+      element = slotsDefault[index];
+    }
+  }
+  return element;
+};
+
+// 获取当前组件的实例
+const instance = getCurrentInstance();
+const { placement, content, width, modelValue } = toRefs(props);
+const isShow = ref(modelValue.value);
+
+// 隐藏tooltip
+function hide(tid: string) {
+  const el = document.getElementById(tid);
+  if (el) {
+    // 移除样式
+    el.classList.remove("h-tooltip-show");
+  }
+};
+
+// 显示tooltip
+function show(tip: any) {
+  tip && tip.classList.add("h-tooltip-show");
+};
+
+function calcStyle(Rect: any, tip: any, key: string): {x: number, y: number} {
+  let y = document.documentElement.scrollTop;
+  let x = 0;
+  const placement:any = {
+    left: () => {
+      x += Rect.x - tip.offsetWidth
+      y += Rect.y + (Rect.height - tip.offsetHeight) * 0.5
+    },
+    right: () => {
+      x += Rect.x + Rect.width
+      y += Rect.y + (Rect.height - tip.offsetHeight) * 0.5
+    },
+    top: () => {
+      x += Rect.x + (Rect.width - tip.offsetWidth) * 0.5
+      y += Rect.y - tip.offsetHeight
+    },
+    bottom: () => {
+      x += Rect.x + (Rect.width - tip.offsetWidth) * 0.5
+      y += Rect.y + Rect.height
+    },
+  };
+  placement[key]();
+  return { x, y };
+}
+
+// 核心函数，获取到已经渲染完毕的DOM.
+function update(tip : any, tid: string): any {
+  // 默认取得插件内第一个元素，并获取它的坐标
+  const Rect = instance?.proxy?.$el.firstElementChild.getBoundingClientRect();
+  const el = document.getElementById(tid);
+  if (!el) {
+    document.body.appendChild(tip);
+  }
+  tip.style.display = "block";
+  const { x, y } = calcStyle(Rect, tip, placement?.value!);
+  tip.style.top = y + "px";
+  tip.style.left = x + "px";
+}
+
+
+onMounted(() => {
+  const el = instance?.proxy?.$el;
+  // 构建以tooltip的DOM元素，并用uid记录
+  const tip = document.createElement("div");
+  tip.className = `h-tooltip h-tooltip-${placement!.value}`;
+  width?.value && (tip.style.width = width.value);
+  const tid = (tip.id = `h-tooltip-${instance!.uid}`);
+  // 监听isShow变化，监听有无接触组件
+
+  watchEffect(() => {
+    tip.innerHTML = `<span>${content?.value}</span>` || "";
+    // 下个宏循环才能获取，加上nextTick
+    nextTick(update(tip, tid));
+
+    if(toString.call(props.modelValue) !== "[object Null]") {
+      isShow.value = modelValue.value;
+    }
+
+    if (isShow.value) {
+      show(tip);
+    } else {
+      hide(tid);
+    }
+  });
+
+  // 监听有无接触组件，接触了为true，离开为false
+  el && el.addEventListener("mouseenter",() => {
+    isShow.value = true;
+  });
+
+  el && el.addEventListener("mouseleave",() => {
+    isShow.value = false;
+  });
+})
+</script>
+<script lang="ts">
 export default defineComponent({
   name: "HTooltip",
-  props: {
-    placement: {
-      type: String,
-      default: 'top',
-      validator: (value: string) => ["right"].includes(value),
-    },
-    width: String,
-    content: [String, Number],
-  },
-  setup(props) {
-    const slots = useSlots();
-    // 获取当前组件的实例
-    const instance = getCurrentInstance();
-    const { placement, content, width } = toRefs(props);
-    const isShow = ref(false);
-
-    // 获取插槽元素
-    function getFirstElement() {
-      const slotsDefault = slots.default &&  slots.default()
-      if (!Array.isArray(slotsDefault)) return null
-      let element = null;
-      for (let index = 0; index < slotsDefault.length; index++) {
-        if (slotsDefault[index] && slotsDefault[index].type) {
-          element = slotsDefault[index];
-        }
-      }
-      return element;
-    };
-
-    // 构建以tooltip的DOM元素，并用uid记录
-    const tip = document.createElement("div");
-    tip.className = `h-tooltip h-tooltip-${placement!.value}`;
-    width?.value && (tip.style.width = width.value)
-    const tid = (tip.id = `h-tooltip-${instance!.uid}`)
-
-    // 隐藏tooltip
-    function hide() {
-      const el = document.getElementById(tid);
-      if (el) {
-        // 移除样式
-        el.classList.remove("h-tooltip-show");
-      }
-    };
-
-    // 显示tooltip
-    function show() {
-      tip && tip.classList.add("h-tooltip-show");
-    };
-
-    function calcStyle(Rect: any, tip: any, key: string): {x: number, y: number} {
-      let y = document.documentElement.scrollTop;
-      let x = 0;
-      const placement:any = {
-        left: () => {
-          x += Rect.x;
-          y += Rect.y - tip.offsetHeight;
-        },
-        right: () => {
-          x += Rect.x + Rect.width
-          y += Rect.y + (Rect.height - tip.offsetHeight) * 0.5
-        },
-      };
-      placement[key]();
-      return { x, y };
-    }
-
-    // 核心函数，获取到已经渲染完毕的DOM.
-    function update() {
-      const Rect = instance?.proxy?.$el.getBoundingClientRect();
-      const el = document.getElementById(tid);
-      if (!el) {
-        document.body.appendChild(tip);
-      }
-      tip.style.display = "block";
-      const { x, y } = calcStyle(Rect, tip, placement?.value!);
-      tip.style.top = y + "px";
-      tip.style.left = x + "px";
-    }
-
-
-    onMounted(() => {
-      const el = instance?.proxy?.$el;
-      // 监听isShow变化，监听有无接触组件
-      watchEffect(() => {
-        tip.innerHTML = `<span>${content?.value}</span>` || "";
-        // 下个宏循环才能获取，加上nextTick
-        nextTick(update);
-
-        if (isShow.value) {
-          show();
-        } else {
-          hide();
-        }
-      });
-
-    // 监听有无接触组件，接触了为true，离开为false
-      el && el.addEventListener("mouseenter",() => {
-        isShow.value = true;
-      });
-
-      el && el.addEventListener("mouseleave",() => {
-        isShow.value = false;
-      });
-    })
-
-    return () => {
-      return getFirstElement()
-    }
-  },
-});
+})
 </script>
