9d1618de42033c658e687d33191bece92725737e
leoyongyuan <1359195795@qq.com>
2023-01-24 22:57:43 +0800

    chore: 清除遗留代码

--- examples/main.ts
+++ examples/main.ts
@@ -1,9 +1,8 @@
-import {createApp} from 'vue'
-import App from './app.vue'
-import router from './router'
-import './style/base.less'
+import { createApp } from "vue";
+import App from "./app.vue";
+import "./style/base.less";
 
-import HViewPlus from '../packages/components/src'
-const app = createApp(App)
+import HViewPlus from "../packages/components/src";
+const app = createApp(App);
 
-app.use(HViewPlus).use(router).mount('#app')
+app.use(HViewPlus).mount("#app");
