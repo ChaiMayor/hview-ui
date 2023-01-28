a3a74bc1b18cccd799d0a55811cbc53616f3eddc
leoyongyuan <1359195795@qq.com>
2023-01-28 23:44:13 +0800

    fix: 解决组件动画不生效的bug

--- packages/components/src/tree/src/transition.vue
+++ packages/components/src/tree/src/transition.vue
@@ -1,5 +1,11 @@
 <template>
-  <transition v-on="on">
+  <transition
+    @before-enter="OnbeforeEnter"
+    @enter="Onenter"
+    @after-enter="OnafterEnter"
+    @before-leave="OnbeforeLeave"
+    @leave="Onleave"
+    @after-leave="OnafterLeave">
     <slot />
   </transition>
 </template>
@@ -18,66 +24,66 @@ export default defineComponent({
       default: "ease",
     },
   },
-
   setup(props) {
-    const inlineStyle = (dom: HTMLDivElement, styles: any) => {
-      if (!dom) return;
-      for (const key in Object.keys(styles)) {
-        if (Object.prototype.hasOwnProperty.call(styles, key)) {
-          dom.style[key] = styles[key];
-        }
-      }
-    };
     const getStyle = (el: any) => {
       if (el.currentStyle) {
         return el.currentStyle;
       }
       return el.currentStyle || getComputedStyle(el);
     };
-    const transition = (config: any) => {
-      const style = {
-        transition: `all ${config.duration}s ${config.animate}`,
-        overflow: "hidden",
-      };
-      return {
-        beforeEnter(el: HTMLDivElement) {
-          el.dataset.paddingTop = getStyle(el).paddingTop; // el.style.paddingTop
-          el.dataset.paddingBottom = getStyle(el).paddingBottom; // el.style.paddingBottom
-          inlineStyle(el, Object.assign(style, { height: 0 }));
-        },
-        enter(el: HTMLDivElement) {
-          setTimeout(() => {
-            inlineStyle(el, {
-              height: `${el.scrollHeight}px`,
-              paddingTop: el.dataset.paddingTop,
-              paddingBottom: el.dataset.paddingBottom,
-            });
-          }, 0);
-        },
-        afterEnter(el: HTMLDivElement) {
-          inlineStyle(el, { height: "", overflow: "" });
-        },
-        beforeLeave(el: HTMLDivElement) {
-          inlineStyle(
-            el,
-            Object.assign(style, {
-              height: `${el.scrollHeight}px`,
-              overflow: "",
-            }),
-          );
-        },
-        leave(el: HTMLDivElement) {
-          setTimeout(() => {
-            inlineStyle(el, { height: 0, overflow: "hidden" });
-          }, 0);
-        },
-        afterLeave(el: HTMLDivElement) {
-          inlineStyle(el, { height: 0, overflow: "hidden" });
-        },
-      };
+    const inlineStyle = (dom: HTMLDivElement, styles: any) => {
+      if (!dom) return;
+      for (const key in styles) {
+        if (Object.prototype.hasOwnProperty.call(styles, key)) {
+          (dom.style as any)[key] = styles[key];
+        }
+      }
+    };
+    const style = {
+      transition: `all ${props.duration}s ${props.animate}`,
+      overflow: "hidden",
+    };
+    const OnbeforeEnter = (el: HTMLDivElement) => {
+      el.dataset.paddingTop = getStyle(el).paddingTop;
+      el.dataset.paddingBottom = getStyle(el).paddingBottom;
+      inlineStyle(el, Object.assign(style, { height: 0 }));
+    };
+    const Onenter = (el: HTMLDivElement) => {
+      setTimeout(() => {
+        inlineStyle(el, {
+          height: `${el.scrollHeight}px`,
+          paddingTop: el.dataset.paddingTop,
+          paddingBottom: el.dataset.paddingBottom,
+        });
+      }, 0);
+    };
+    const OnafterEnter = (el: HTMLDivElement) => {
+      inlineStyle(el, { height: "", overflow: "" });
+    };
+    const OnbeforeLeave = (el: HTMLDivElement) => {
+      inlineStyle(
+        el,
+        Object.assign(style, {
+          height: `${el.scrollHeight}px`,
+          overflow: "",
+        }),
+      );
+    };
+    const Onleave = (el: HTMLDivElement) => {
+      setTimeout(() => {
+        inlineStyle(el, { height: 0, overflow: "hidden" });
+      }, 0);
+    };
+    const OnafterLeave = (el: HTMLDivElement) => {
+      inlineStyle(el, { height: 0, overflow: "hidden" });
     };
     return {
-      on: transition(props),
+      OnbeforeEnter,
+      Onenter,
+      OnafterEnter,
+      OnbeforeLeave,
+      Onleave,
+      OnafterLeave,
     };
   },
 });
