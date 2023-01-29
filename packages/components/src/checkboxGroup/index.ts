c448cdb26046682fb16c736ffcf87498c7448a07
leoyongyuan <1359195795@qq.com>
2023-01-29 22:33:14 +0800

    feat: 新建checkboxGroup组件并引入install

---
+++ packages/components/src/checkboxGroup/index.ts
@@ -0,0 +1,5 @@
+import checkboxGroup from "./src/checkboxGroup.vue";
+import { withInstall } from "@hview-plus/utils";
+
+const CheckboxGroup = withInstall(checkboxGroup);
+export default CheckboxGroup;
