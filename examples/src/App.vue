83abcd75497fc8b52eb788bdfc1bc3a3ba3ae727
JadeLM <205329660@qq.com>
2023-01-30 10:14:05 +0800

    feat: 新增result组件

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="container">
+    <h-result title="404" sub-title="Sorry, request error">
+      <template #icon>
+        <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" />
+      </template>
+      <template #extra>
+        <h-button type="primary">Back</h-button>
+      </template>
+    </h-result>
     <h-result icon="success" title="Success Tip" sub-title="Please follow the instructions">
       <template #extra>
         <h-button type="primary">Back</h-button>
