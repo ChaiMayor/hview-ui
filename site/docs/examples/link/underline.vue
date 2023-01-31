87d6e6656d54d88a6b14405144f491d71de23468
leoyongyuan <1359195795@qq.com>
2023-01-31 14:50:54 +0800

    feat(docs): 新建link组件的演示demo

---
+++ site/docs/examples/link/underline.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h-link type="primary" class="primary" underline>primary</h-link>
+    <h-link type="success" class="sucesss" underline>success</h-link>
+    <h-link type="warning" class="warning" underline>warning</h-link>
+    <h-link type="danger" class="danger" underline>danger</h-link>
+    <h-link type="info" class="info" underline>info</h-link>
+  </div>
+</template>
