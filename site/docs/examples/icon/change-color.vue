465a93935ee1923f26736361e59d9e7102e7457d
leoyongyuan <1359195795@qq.com>
2023-01-31 16:18:28 +0800

    docs: 新增icon组件展示列表

---
+++ site/docs/examples/icon/change-color.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="icon-example">
+    <h-icon name="edit" color="red"></h-icon>
+    <h-icon name="share" color="#fff159"></h-icon>
+    <h-icon name="delete" color="blue"></h-icon>
+    <h-icon name="success" color="#67C23A"></h-icon>
+  </div>
+</template>
+<style scoped>
+.icon-example {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  font-size: 30px;
+}
+</style>
---
+++ site/docs/examples/icon/icon-list.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="icon_content">
+    <div class="icon_list" v-for="(icon, index) in iconList" :key="index">
+      <h-icon :name="icon"></h-icon>
+      <div class="icon_name">{{ icon }}</div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { iconList } from "./data";
+</script>
+<style scoped>
+.icon_content {
+  overflow: hidden;
+  border-top: 1px solid #f5f5f5;
+  border-left: 1px solid #f5f5f5;
+}
+.icon_content .icon_list {
+  float: left;
+  width: 20%;
+  height: 110px;
+  border-right: 1px solid #f5f5f5;
+  border-bottom: 1px solid #f5f5f5;
+  padding: 20px;
+  display: flex;
+  font-size: 24px;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  box-sizing: border-box;
+  cursor: pointer;
+}
+
+.icon_name {
+  color: gray;
+  font-size: 12px;
+  width: 100%;
+  text-align: center;
+}
+</style>
