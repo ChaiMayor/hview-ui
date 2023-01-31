e0c77350644f43aef7f12651724c17d77578c243
leoyongyuan <1359195795@qq.com>
2023-01-31 14:05:12 +0800

    docs(example): 增加button组件的演示demo

---
+++ site/docs/examples/button/basic.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h-button>Default</h-button>
+    <h-button type="primary">Primary</h-button>
+    <h-button type="success">Success</h-button>
+    <h-button type="info">Info</h-button>
+    <h-button type="warning">Warning</h-button>
+    <h-button type="danger">Danger</h-button>
+  </div>
+  <div>
+    <h-button plain>Plain</h-button>
+    <h-button type="primary" plain>Primary</h-button>
+    <h-button type="success" plain>Success</h-button>
+    <h-button type="info" plain>Info</h-button>
+    <h-button type="warning" plain>Warning</h-button>
+    <h-button type="danger" plain>Danger</h-button>
+  </div>
+  <div>
+    <h-button round>Round</h-button>
+    <h-button type="primary" round>Primary</h-button>
+    <h-button type="success" round>Success</h-button>
+    <h-button type="info" round>Info</h-button>
+    <h-button type="warning" round>Warning</h-button>
+    <h-button type="danger" round>Danger</h-button>
+  </div>
+</template>
---
+++ site/docs/examples/button/icon.vue
@@ -0,0 +1,6 @@
+<template>
+  <h-button icon="edit" type="primary"></h-button>
+  <h-button icon="edit" type="primary" plain>编辑</h-button>
+  <h-button icon="share" type="primary" round></h-button>
+  <h-button icon="share" type="success">分享</h-button>
+</template>
