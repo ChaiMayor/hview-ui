035edc3944ac3521408bb1aefd589846fdc251e3
desain <1810479856@qq.com>
2023-01-31 17:15:25 +0800

    docs: 修改完善 card 中英文文档

---
+++ site/docs/examples/card/basic.vue
@@ -0,0 +1,21 @@
+<template>
+  <h-card class="card">
+    <template #header>
+      <div class="card-header">
+        <div>Title</div>
+      </div>
+    </template>
+    <div v-for="i in 4" :key="i">{{ "Content " + i }}</div>
+    <template #handle>
+      <div class="card-handle">
+        <h-button>handle</h-button>
+      </div>
+    </template>
+  </h-card>
+</template>
+
+<style>
+.card {
+  width: 400px;
+}
+</style>
---
+++ site/docs/examples/card/pictureCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <h-card class="card" :body-style="{ padding: '0px' }">
+    <img src="https://t8.baidu.com/it/u=1876327120,31941088&fm=193" />
+  </h-card>
+</template>
+
+<style>
+.card {
+  width: 400px;
+}
+</style>
