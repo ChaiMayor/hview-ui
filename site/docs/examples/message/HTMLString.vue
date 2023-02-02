4ed3f9cab8ad15f46773ee0543f5dc7dd43ea14c
leoyongyuan <1359195795@qq.com>
2023-02-02 14:18:17 +0800

    docs: 新建message组件5个demo

---
+++ site/docs/examples/message/HTMLString.vue
@@ -0,0 +1,16 @@
+<template>
+  <h-button type="default" @click="handerClick">Use HTML String</h-button>
+</template>
+
+<script setup lang="ts">
+import { getCurrentInstance } from "vue";
+const instance = getCurrentInstance();
+const { $message } = instance.appContext.config.globalProperties;
+const handerClick = () => {
+  $message({
+    type: "success",
+    message: "<strong>This is HTML string</strong>",
+    dangerouslyUseHTMLString: true,
+  });
+};
+</script>
