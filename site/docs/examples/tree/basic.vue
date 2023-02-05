147fb06b8b7495709953be8ced7fa5108e168aeb
leoyongyuan <1359195795@qq.com>
2023-02-05 15:48:21 +0800

    docs: 新增tree组件的六个demo

---
+++ site/docs/examples/tree/basic.vue
@@ -0,0 +1,58 @@
+<template>
+  <h-tree :data="list"></h-tree>
+</template>
+
+<script setup lang="ts">
+const list = [
+  {
+    label: "Level one 1",
+    children: [
+      {
+        label: "Level two 1-1",
+        children: [
+          {
+            label: "Level three 1-1-1",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: "Level one 2",
+    children: [
+      {
+        label: "Level two 2-1",
+        children: [
+          {
+            label: "Level three 2-1-1",
+          },
+        ],
+      },
+      {
+        label: "Level two 2-2",
+      },
+    ],
+  },
+  {
+    label: "Level one 3",
+    children: [
+      {
+        label: "Level two 3-1",
+        children: [
+          {
+            label: "Level three 3-1-1",
+          },
+        ],
+      },
+      {
+        label: "Level two 3-2",
+        children: [
+          {
+            label: "Level three 3-2-1",
+          },
+        ],
+      },
+    ],
+  },
+];
+</script>
