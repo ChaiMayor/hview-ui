3b2bd8eb90c46b928aee1709a82a257f88717a80
ChaiMayor <2521630545@qq.com>
2023-02-08 11:07:49 +0800

    text: 测试 lint-staged 配置文件

---
+++ .lintstagedrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+	"*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write", "eslint"],
+	"{!(package)*.json,*.code-snippets,.!(browserslist)*rc}": [
+		"prettier --write--parser json",
+	],
+	"package.json": ["prettier --write"],
+	"*.vue": ["prettier --write", "eslint --fix", "eslint"],
+	"packages/theme-chalk/src/**/*.{less,css}": [
+		"stylelint --fix --custom-syntax postcss-less",
+		"prettier --write",
+	],
+	"*.md": ["prettier --write"],
+	"*.*": ["ls-lint"],
+};
