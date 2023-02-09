a8ae26de3623970c17c09a3d19dc1f2c69a960a2
leoyongyuan <1359195795@qq.com>
2023-02-09 16:46:22 +0800

    test: 更新App，测试switch组件

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -1,12 +1,35 @@
 <template>
-  <h-button type="default" @click="handerClick">Show Message</h-button>
+  <h-switch v-model="switchVal1" active-text="启用" inactive-text="停用"> </h-switch>
+  <h-switch
+    v-model="switchVal2"
+    inline-prompt
+    size="large"
+    active-icon="close1"
+    inactive-icon="success1"
+    style="display: block; margin-top: 10px">
+  </h-switch>
+  <h-switch
+    v-model="switchVal3"
+    inline-prompt
+    active-text="Y"
+    inactive-text="N"
+    style="display: block; margin-top: 10px">
+  </h-switch>
+  <h-switch
+    v-model="switchVal4"
+    inline-prompt
+    active-text="开"
+    inactive-text="关"
+    active-color="#13ce66"
+    inactive-color="#ff4949"
+    style="display: block; margin-top: 10px">
+  </h-switch>
 </template>
 
 <script setup lang="ts">
-import { getCurrentInstance } from "vue";
-const instance = getCurrentInstance();
-const { $message } = instance.appContext.config.globalProperties;
-const handerClick = () => {
-  $message("Hello,This is a message");
-};
+import { ref } from "vue";
+const switchVal1 = ref(true);
+const switchVal2 = ref(true);
+const switchVal3 = ref(true);
+const switchVal4 = ref(true);
 </script>
