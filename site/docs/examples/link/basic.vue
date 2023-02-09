244e70125bed5c3af7f6553bf835117f293bcb50
leoyongyuan <1359195795@qq.com>
2023-02-09 16:44:27 +0800

    docs: 修改文档demo

--- site/docs/examples/link/basic.vue
+++ site/docs/examples/link/basic.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h-link type="primary" class="primary">primary</h-link>
+    <h-link type="primary" href="https://github.com/ChaiMayor/hview-ui" class="primary">primary</h-link>
     <h-link type="success" class="success">success</h-link>
     <h-link type="warning" class="warning">warning</h-link>
     <h-link type="danger" class="danger">danger</h-link>
