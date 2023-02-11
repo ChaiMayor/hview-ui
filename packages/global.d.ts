c10c69b426490acb2294fd072a387d2ab1f973c4
ChaiMayor <2521630545@qq.com>
2023-02-11 18:06:28 +0800

    chore: 更新打包的 ts 声明文件

--- packages/global.d.ts
+++ packages/global.d.ts
@@ -1,7 +1,9 @@
 export {};
 declare module "@vue/runtime-core" {
+  import type { MessageOptions } from "./components/message/src/index";
   interface ComponentCustomProperties {
     // refer: https://www.cnblogs.com/it774274680/p/16660035.html
     $t: (str: string) => string;
+    $message: (options: MessageOptions) => void;
   }
 }
