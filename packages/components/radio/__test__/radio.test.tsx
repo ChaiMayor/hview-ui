3e72b31762258f9744e86b398e2ed7baa1ffc1a3
ChaiMayor <2521630545@qq.com>
2023-02-16 10:21:12 +0800

    test: radio 测试文件

---
+++ packages/components/radio/__test__/radio.test.tsx
@@ -0,0 +1,72 @@
+import { ref } from "vue";
+import { mount } from "@vue/test-utils";
+import { describe, expect, test } from "vitest";
+import Radio from "../src/radio.vue";
+import { OptionsItem } from "../src/radio";
+
+describe("InputNumber.vue", () => {
+  test("create", async () => {
+    const change = () => {};
+
+    const value = ref<string>("1");
+    const options = ref<OptionsItem[]>([
+      {
+        label: "Option 1",
+        id: 1,
+      },
+      {
+        label: "Option 2",
+        id: 2,
+      },
+    ]);
+
+    const wrapper = mount(
+      <Radio v-model={value.value} options={options.value} onChange={change} />,
+    );
+
+    wrapper.vm.change("Option 1", 1);
+    wrapper.vm.inlineStyle();
+    expect(wrapper.find(".h-radio-item-button").exists()).toBe(true);
+  });
+
+  test("inline to false", async () => {
+    const value = ref<string>("1");
+    const options = ref<OptionsItem[]>([
+      {
+        label: "Option 1",
+        id: 1,
+      },
+      {
+        label: "Option 2",
+        id: 2,
+      },
+    ]);
+
+    mount(
+      <Radio v-model={value.value} options={options.value} inline={false} />,
+    );
+  });
+});
+
+// test("disabled", async () => {
+//   const change = () => {};
+
+//   const value = ref<string>("2");
+//   const options = ref<OptionsItem[]>([
+//     {
+//       label: "Option 1",
+//       id: 1,
+//     },
+//     {
+//       label: "Option 2",
+//       id: 2,
+//       disabled: false,
+//     },
+//   ]);
+
+//   const wrapper = mount(
+//     <Radio v-model={value.value} options={options.value} onChange={change} />,
+//   );
+
+//   wrapper.vm.change("Option 2", 2);
+// });
