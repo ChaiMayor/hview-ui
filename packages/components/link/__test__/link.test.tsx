3eb2f08b9f27fcf7aa9f8281ad1b31259c37c6c9
leoyongyuan <1359195795@qq.com>
2023-06-25 10:04:02 +0800

    test: 补充button组件单元测试

--- packages/components/link/__test__/link.test.tsx
+++ packages/components/link/__test__/link.test.tsx
@@ -6,18 +6,51 @@ import { LinkType } from "../src/link";
 
 describe("Link.vue", () => {
   it("type", () => {
-    const wrapper = mount(Link, {
-      props: {
-        type: LinkType[1],
-      },
-    });
+    LinkType.forEach((type) => {
+      const wrapper = mount(Link, {
+        props: {
+          type,
+        },
+      });
 
-    expect(wrapper.classes()).toContain("h-link--success");
+      expect(wrapper.classes()).toContain(`h-link--${type}`);
+    });
   });
 
   it("disabled", async () => {
     const wrapper = mount(() => <Link disabled />);
 
     expect(wrapper.classes()).toContain("is-disabled");
+  });
+
+  it("underline", async () => {
+    const wrapper = mount(() => <Link underline />);
+
+    expect(wrapper.classes()).toContain("is-underline");
+  });
+
+  it("icon", async () => {
+    const wrapper = mount(() => <Link icon="fuzhi" />);
+
+    expect(wrapper.find(".h-icon-fuzhi").exists());
+  });
+
+  it("click", async () => {
+    const wrapper = mount(() => <Link />);
+
+    await wrapper.trigger("click");
+    expect(wrapper.emitted("click")).toBeDefined();
+  });
+
+  it("slot", async () => {
+    const wrapper = mount({
+      setup: () => () => <Link>Hello world</Link>,
+    });
+    expect(wrapper.text()).toContain("Hello world");
+  });
+
+  it("href", async () => {
+    const wrapper = mount(() => <Link href="https://www.baidu.com" />);
+    expect(wrapper.attributes("href")).toBe("https://www.baidu.com");
   });
 });
