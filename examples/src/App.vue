a75384ff7cb4247c4d843e4ff9443821f609e44f
leoyongyuan <1359195795@qq.com>
2023-07-22 21:36:29 +0800

    docs: 新增color-picker组件演示demo

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -1,58 +1,8 @@
 <template>
-  <h-tree :data="list" isLazy></h-tree>
+  <h-color-picker v-model="value"></h-color-picker>
 </template>
 
 <script setup lang="ts">
-const list = [
-  {
-    label: "Level one 1",
-    children: [
-      {
-        label: "Level two 1-1",
-        children: [
-          {
-            label: "Level three 1-1-1",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    label: "Level one 2",
-    children: [
-      {
-        label: "Level two 2-1",
-        children: [
-          {
-            label: "Level three 2-1-1",
-          },
-        ],
-      },
-      {
-        label: "Level two 2-2",
-      },
-    ],
-  },
-  {
-    label: "Level one 3",
-    children: [
-      {
-        label: "Level two 3-1",
-        children: [
-          {
-            label: "Level three 3-1-1",
-          },
-        ],
-      },
-      {
-        label: "Level two 3-2",
-        children: [
-          {
-            label: "Level three 3-2-1",
-          },
-        ],
-      },
-    ],
-  },
-];
+import { ref } from "vue";
+const value = ref("");
 </script>
---
+++ site/docs/examples/color-picker/rgba.vue
@@ -0,0 +1,8 @@
+<template>
+  <h-color-picker v-model="value" RGB></h-color-picker>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const value = ref("");
+</script>
